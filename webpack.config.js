e204e6f820c7fbc748ef02afe9b37f5be1103655
su- <aaaiiiuuu35@gmail.com>
2020-05-02 18:42:49 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
+
+module.exports = {
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+  },
+  entry: {
+    main: ['@babel/polyfill', path.resolve(__dirname, './src/index.ts')],
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx|ts|tsx)$/,
+        include: path.resolve('./src'),
+        exclude: /(node_modules|dist)/,
+        loader: 'eslint-loader',
+        options: {
+          fix: true,
+        },
+      },
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        use: ['thread-loader', 'cache-loader', 'babel-loader'],
+        include: path.resolve('./src'),
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    plugins: [],
+  },
+  plugins: [new HardSourceWebpackPlugin()],
+};
