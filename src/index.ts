141e9ec2a41433bb3e3c1aa5d80c82e2bf73362f
su- <aaaiiiuuu35@gmail.com>
2020-05-02 20:40:42 +0900

    ast読み込み

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import { readFile } from '@/util/readFile';
+import { readFile, writeAstToJson } from '@/util/file';
 import { requireCaption } from '@/rules/requireCaption';
 
+import * as fs from 'fs';
+
+import { latexParser } from 'latex-utensils';
+const texString = fs.readFileSync('./tex/uno.tex', { encoding: 'utf-8' });
+const ast = latexParser.parse(texString);
+console.log(JSON.stringify(ast, undefined, '  '));
+
+writeAstToJson(ast);
+
 const main = () => {
-  const lines = readFile('./tex/uno.tex');
+  const lines = file('./tex/uno.tex');
   requireCaption(lines);
 };
 
-main();
+// main();
--- src/util/file.ts
+++ src/util/file.ts
@@ -4,3 +4,11 @@ export const readFile = (filePath: string): string[] => {
   const text = fs.readFileSync(filePath, { encoding: 'utf-8' });
   return text.toString().split('\n');
 };
+
+export const writeAstToJson = (ast: any): void => {
+  fs.writeFile('outDir/out.json', JSON.stringify(ast, undefined, 2), err => {
+    if (err) {
+    } else {
+    }
+  });
+};
