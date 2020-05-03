8fd7c3801c35cb1f31e7f303976235a473aaf860
su- <aaaiiiuuu35@gmail.com>
2020-05-03 14:53:05 +0900

    ひな形

--- src/rules/requireLabelRef.ts
+++ src/rules/requireLabelRef.ts
@@ -1,1 +1,13 @@
 // ラベル参照を必須にするルール
+
+export const labelsList: Array<{
+  labelName: string,
+  node: any,
+}> = [];
+
+export const addLabel = (labelName: string, node: any) => {
+  labelsList.push({
+    labelName,
+    node,
+  });
+};
