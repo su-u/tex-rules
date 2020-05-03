18dc474cf642069dfb52723d3195662a42dd91d2
su- <aaaiiiuuu35@gmail.com>
2020-05-03 11:36:09 +0900

    テスト修正

--- src/__tests__/requireCaption.test.ts
+++ src/__tests__/requireCaption.test.ts
@@ -1,9 +1,15 @@
 import { requireCaption } from '../rules/requireCaption';
-import ast from './testAstJson/figure_has_caption.json';
 
 describe('requireCaption', (): void => {
-  test('', (): void => {
-    const hasCaption = !requireCaption([ast]);
+  test('has', (): void => {
+    const ast = require('./testAstJson/figure_has_caption.json');
+    const hasCaption = requireCaption([ast]);
     expect(hasCaption).toBeTruthy();
+  });
+
+  test('has not', (): void => {
+    const ast = require('./testAstJson/figure_has_not_caption.json');
+    const hasCaption = requireCaption([ast]);
+    expect(hasCaption).toBeFalsy();
   });
 });
