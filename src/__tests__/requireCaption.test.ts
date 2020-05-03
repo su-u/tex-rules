4f6830863aa4038eb660e53519bfbb06634ea840
su- <aaaiiiuuu35@gmail.com>
2020-05-03 11:41:38 +0900

    jestの設定追加

--- src/__tests__/requireCaption.test.ts
+++ src/__tests__/requireCaption.test.ts
@@ -1,13 +1,13 @@
-import { requireCaption } from '../rules/requireCaption';
+import { requireCaption } from '@/rules/requireCaption';
 
 describe('requireCaption', (): void => {
-  test('has', (): void => {
+  test('has caption', (): void => {
     const ast = require('./testAstJson/figure_has_caption.json');
     const hasCaption = requireCaption([ast]);
     expect(hasCaption).toBeTruthy();
   });
 
-  test('has not', (): void => {
+  test('has not caption', (): void => {
     const ast = require('./testAstJson/figure_has_not_caption.json');
     const hasCaption = requireCaption([ast]);
     expect(hasCaption).toBeFalsy();
