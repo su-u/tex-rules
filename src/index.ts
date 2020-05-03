e99dd234fb9d364da23cdc4eae115a0ad64713e0
su- <aaaiiiuuu35@gmail.com>
2020-05-03 14:14:35 +0900

    label必須機能追加

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { readFile, writeAstToJson } from '@/util/file';
 import { TexToAst, getDocument } from '@/util/tex';
 import { requireCaption, requireCaptionsList } from '@/rules/requireCaption';
-import { report, reportOutput } from '@/./report';
+import { requireLabel, requireLabelsList } from '@/rules/requireLabel';
+import { report, reportOutput } from '@/report';
 
 const switcher = (ast: any) => {
   ast.forEach((node: any) => {
@@ -11,11 +12,22 @@ const switcher = (ast: any) => {
       }
       case 'env': {
         if (requireCaptionsList.includes(node.name)) {
-          const hasCaption = requireCaption(node.content);
-          if (!hasCaption) {
+          if (requireCaption(node.content)) {
             const location = node.location;
             report(
               `${node.name}にキャプションがありません`,
+              'error',
+              node.name,
+              location.start.line,
+              location.start.column,
+            );
+          }
+        }
+        if (requireLabelsList.includes(node.name)) {
+          if (requireLabel(node.content)) {
+            const location = node.location;
+            report(
+              `${node.name}にラベルがありません`,
               'error',
               node.name,
               location.start.line,
--- src/rules/requireLabel.ts
+++ src/rules/requireLabel.ts
@@ -1,11 +1,11 @@
 // eslint-disable-next-line no-undef
-export const requireCaptionsList: ReadonlyArray<string> = ['figure', 'table'];
+export const requireLabelsList: ReadonlyArray<string> = ['figure', 'table'];
 
-export const requireCaption = (ast: any): number => {
+export const requireLabel = (ast: any): number => {
   return ast.some((node: any) => {
     switch (node.kind) {
       case 'env': {
-        return requireCaption(node.content);
+        return requireLabel(node.content);
       }
       case 'command': {
         if (node.name === 'caption') {
