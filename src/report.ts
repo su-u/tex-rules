288c2c38b2148e68c8591da9781bbf1189833bf0
su- <aaaiiiuuu35@gmail.com>
2020-05-03 16:49:32 +0900

    リファックタリング

--- src/report.ts
+++ src/report.ts
@@ -4,7 +4,7 @@ export const reportList: reportType[] = [];
 
 export type reportKey = 'error' | 'info';
 
-export const reportTypeObject: { [key in reportKey]: string } = {
+export const reportOutputTextColor: { [key in reportKey]: string } = {
   error: RED,
   info: WHITE,
 };
@@ -32,7 +32,7 @@ export const reportOutput = () => {
     // eslint-disable-next-line no-console
     console.log(
       `${WHITE}${('  ' + report.line).substr(-3)}:${report.column} ${
-        reportTypeObject[report.reportType]
+        reportOutputTextColor[report.reportType]
       }${report.reportType}${BLACK} ${report.errorText}\t${WHITE}${
         report.nodeName
       }${RESET}`,
