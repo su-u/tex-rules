aefb94d892831b3b5a8f47a537b55d6e63529d85
su- <aaaiiiuuu35@gmail.com>
2020-05-03 14:41:19 +0900

    コード整形

--- src/__tests__/requireCaption/requireCaption.test.ts
+++ src/__tests__/requireCaption/requireCaption.test.ts
@@ -4,29 +4,37 @@ import { texToDocumentAst } from '../../util/test';
 describe('requireCaption', (): void => {
   describe('figure', (): void => {
     test('has caption', (): void => {
-      const ast = texToDocumentAst('./src/__tests__/requireCaption/tex/figure_has_caption.tex');
+      const ast = texToDocumentAst(
+        './src/__tests__/requireCaption/tex/figure_has_caption.tex',
+      );
       const hasCaption = requireCaption([ast]);
       expect(hasCaption).toBeTruthy();
     });
 
     test('has not caption', (): void => {
-      const ast = texToDocumentAst('./src/__tests__/requireCaption/tex/figure_has_not_caption.tex');
+      const ast = texToDocumentAst(
+        './src/__tests__/requireCaption/tex/figure_has_not_caption.tex',
+      );
       const hasCaption = requireCaption([ast]);
       expect(hasCaption).toBeFalsy();
     });
-  })
+  });
 
   describe('table', (): void => {
     test('has table', (): void => {
-      const ast = texToDocumentAst('./src/__tests__/requireCaption/tex/table_has_caption.tex');
+      const ast = texToDocumentAst(
+        './src/__tests__/requireCaption/tex/table_has_caption.tex',
+      );
       const hasCaption = requireCaption([ast]);
       expect(hasCaption).toBeTruthy();
     });
 
     test('has not table', (): void => {
-      const ast = texToDocumentAst('./src/__tests__/requireCaption/tex/table_has_not_caption.tex');
+      const ast = texToDocumentAst(
+        './src/__tests__/requireCaption/tex/table_has_not_caption.tex',
+      );
       const hasCaption = requireCaption([ast]);
       expect(hasCaption).toBeFalsy();
     });
-  })
+  });
 });
