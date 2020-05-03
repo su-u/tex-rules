586506fd06240796a80ccfb5f65c1d84c6da2157
su- <aaaiiiuuu35@gmail.com>
2020-05-03 16:28:06 +0900

    ラベルの参照を必須に

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,35 @@ import { readFile, writeAstToJson } from '@/util/file';
 import { TexToAst, getDocument } from '@/util/tex';
 import { requireCaption, requireCaptionsList } from '@/rules/requireCaption';
 import { requireLabel, requireLabelsList } from '@/rules/requireLabel';
+import { LabelRefClass } from '@/rules/requireLabelRef';
 import { report, reportOutput } from '@/report';
 
-const switcher = (ast: any) => {
-  ast.forEach((node: any) => {
-    switch (node.kind) {
-      case 'parbreak': {
-        break;
-      }
-      case 'env': {
-        if (requireCaptionsList.includes(node.name)) {
-          if (!requireCaption(node.content)) {
-            report(`${node.name}にキャプションがありません`, 'error', node);
-          }
-        }
-        if (requireLabelsList.includes(node.name)) {
-          if (!requireLabel(node.content)) {
-            report(`${node.name}にラベルがありません`, 'error', node);
-          }
-        }
-      }
+const labelRef = new LabelRefClass();
+
+const interpreter = (node: any) => {
+  switch (node.kind) {
+    case 'parbreak': {
+      break;
     }
-  });
+    case 'env': {
+      if (requireCaptionsList.includes(node.name)) {
+        if (!requireCaption(node.content)) {
+          report(`${node.name}にキャプションがありません`, 'error', node);
+        }
+      }
+      if (requireLabelsList.includes(node.name)) {
+        if (!requireLabel(node.content)) {
+          report(`${node.name}にラベルがありません`, 'error', node);
+        }
+      }
+      break;
+    }
+    case 'command': {
+      if (node.name === 'label') labelRef.addLabel(node);
+      if (node.name === 'ref') labelRef.addRef(node);
+      break;
+    }
+  }
 };
 
 const main = () => {
@@ -32,7 +39,16 @@ const main = () => {
   const documentAst = getDocument(ast);
   writeAstToJson('outDir/out.json', ast);
   writeAstToJson('outDir/document.json', documentAst);
-  switcher(documentAst.content);
+
+  documentAst.content.forEach((node: any) => interpreter(node));
+
+  labelRef.labelAggregate(report);
+  labelRef.labelsList.forEach(y => {
+    console.debug(y);
+  });
+  labelRef.refsList.forEach(y => {
+    console.debug(y);
+  });
   reportOutput();
 };
 
--- src/rules/requireLabelRef.ts
+++ src/rules/requireLabelRef.ts
@@ -1,13 +1,57 @@
 // ラベル参照を必須にするルール
+import { reportKey } from '@/report';
 
-export const labelsList: Array<{
-  labelName: string,
-  node: any,
-}> = [];
-
-export const addLabel = (labelName: string, node: any) => {
-  labelsList.push({
-    labelName,
-    node,
-  });
+type labelType = {
+  labelName: string;
+  node: any;
+  count: number;
 };
+
+type refType = {
+  refName: string;
+  node: any;
+};
+
+export class LabelRefClass {
+  labelsList: Array<labelType>;
+  refsList: Array<refType>;
+
+  constructor() {
+    this.labelsList = [];
+    this.refsList = [];
+  }
+
+  addLabel = (node: any) => {
+    this.labelsList.push({
+      labelName: node.args[0].content[0].content,
+      node,
+      count: 0,
+    });
+  };
+
+  addRef = (node: any) => {
+    this.refsList.push({
+      refName: node.args[0].content[0].content,
+      node,
+    });
+  };
+
+  labelAggregate = (
+    report: (errorText: string, reportType: reportKey, node: any) => void,
+  ) => {
+    this.labelsList.forEach((label: labelType, index, array) => {
+      array[index] = {
+        ...label,
+        count: this.refsList.filter(ref => ref.refName === label.labelName)
+          .length,
+      };
+      if (array[index].count === 0) {
+        report(
+          `ラベル「${label.labelName}」は参照されていません。`,
+          'error',
+          label.node,
+        );
+      }
+    });
+  };
+}
